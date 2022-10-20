c6e6611075c849cd959f03ab9ad93b9f7a0ef3f5
huakucha <zhuyuliangm@outlook.com>
2022-10-20 16:51:37 +0800

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-import dayjs from "dayjs";
+import dayjs from "dayjs"
 
 export function formatDate(date: string | Date, year = true) {
-  return year ? dayjs(date).format("MMM D, YYYY") : dayjs(date).format("MMM D");
+  return year ? dayjs(date).format("YYYY-MM-DD") : dayjs(date).format("MM-DD")
 }

5b9f7eb56b82504669f6d3e7fc0f78c34dda6ab0
huakucha <zhuyuliangm@outlook.com>
2022-11-04 16:59:51 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,27 @@ if(routes.length > 0) {
   })
 }
 
+
+
+customElements.define('copy-code', class extends HTMLElement {
+      /* 当test-shadow-root组件被挂载到DOM上时，执行构造函数 */
+      constructor() {
+        super()
+        const shadowRoot = this.attachShadow({mode: 'open'}) //给指定的元素挂载影子DOM
+        // 当执行 this.attachShadow()方法时，shadowRoot被挂载构造函数中，可以通过this访问
+        // mode open shadow root元素可以从js外部访问根节点
+        // mode closed  拒绝从js外部访问关闭的shadow root节点
+        // console.log('执行', this)
+        const div = document.createElement('div')
+        div.className = 'codelang'
+        div.textContent = '我是div的内容'
+        // shadowRoot.appendChild()
+        // console.log('this', this.shadowRoot)
+        shadowRoot.appendChild(div)
+        // this.shadowRoot === shadowRoot  true
+      }
+    })
+
 </script>
 
 <template>

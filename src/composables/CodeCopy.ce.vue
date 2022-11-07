571aa960681bad5a63994bff6dfb0b159547c168
huakucha <zhuyuliangm@outlook.com>
2022-11-07 15:27:21 +0800

    add copy(TODO)

--- src/composables/CodeCopy.ce.vue
+++ src/composables/CodeCopy.ce.vue
@@ -3,15 +3,32 @@
   defineProps({
     lang: String
   })
+  const copyMsg = $ref('copy')
+  const doCopy = () => {
+    copyMsg = 'copied'
+    setTimeout(() => {
+      copyMsg =  'copy'
+    }, 5000)
+  }
 </script>
 <template>
-  <span class="codeLang">
-    {{lang}}
-  </span>
+  <div>
+    <span  class="copyBtn" @click="doCopy">
+      {{copyMsg}}
+    </span>
+    <span class="codeLang">
+      {{lang}}
+    </span>
+  </div>
 </template>
 
 <style scoped>
 .codeLang {
-  @apply float-right text-gray-300 dark:text-gray-500 text-base;
+  @apply float-right text-gray-300 dark:text-gray-500 text-base items-baseline mr-2;
 }
+.copyBtn {
+  @apply float-right text-gray-300 dark:text-gray-500 text-base items-baseline cursor-pointer;
+  @apply dark:border-gray-600 dark:text-gray-400 dark:bg-gray-800 hover:text-blue-700 dark:hover:text-white
+}
+
 </style>

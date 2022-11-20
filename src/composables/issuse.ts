5e6d2e7fac6bcc342ff9a036a2986399f5caa238
huakucha <zhuyuliangm@outlook.com>
2022-11-20 09:32:06 +0800

    fix

--- src/composables/issuse.ts
+++ src/composables/issuse.ts
@@ -10,7 +10,7 @@ const fetchConfig:UseFetchOptions  = {
       cancel()
     options.headers = {
       ...options.headers,
-      Authorization: `Bearer ${authToken}`,
+      Authorization: `${authToken}`,
     }
     return {
       options
